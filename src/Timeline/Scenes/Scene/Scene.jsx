96f5efd8357e914d21456f2902e309e81b123f27
abhchand <me@abhchand.me>
2020-05-13 14:47:03 -0400

    Display date in Scene card

--- src/Timeline/Scenes/Scene/Scene.jsx
+++ src/Timeline/Scenes/Scene/Scene.jsx
@@ -10,15 +10,34 @@ class Scene extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+    ];
   }
 
   render() {
     const { scene } = this.props;
+    const month = this.months[parseInt(scene.date.month, 10) - 1];
 
     return (
       <div className="scene" data-id={`${scene.season}-${scene.episode}-${scene.scene}`}>
         <a target="_blank" href={scene.image.original} ><img src={scene.image.thumb} /></a>
         <h4 className="episode">S{scene.season} E{scene.episode}</h4>
+        <p className="date">
+          {scene.date.day} {month}, {scene.date.year}
+        </p>
         <p className="description">{scene.description}</p>
       </div>
     );
