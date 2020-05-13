035a06dfa6b58fb683aa414d63fb3f300966b127
abhchand <me@abhchand.me>
2020-05-13 16:54:20 -0400

    Adding title card, removing old logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Timeline from './Timeline/Timeline';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="app">
+      <img className="title-card" src={process.env.PUBLIC_URL + '/dark-title-card.png'} width="490px" />
+
       <div className="intro">
-        <h1>Dark Series Timeline</h1>
+        <h1>Explore the Timeline of 'Dark'</h1>
         <p>The TV Series <a href="https://en.wikipedia.org/wiki/Dark_(TV_series)">Dark</a> has a complex storyline that takes place over several time periods.</p>
         <p>Below, you can explore each scene in chronological order of the timeline, or in the televised episode order.</p>
       </div>
