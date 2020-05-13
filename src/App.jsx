73e60f000f885204c77bb2fffc6d83fe784501aa
abhchand <me@abhchand.me>
2020-05-13 10:59:13 -0400

    Intro copy

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
     <div className="app">
       <div className="intro">
         <h1>Dark Series Timeline</h1>
-        <p>The TV Series Dark has a complex storyline that takes place over several time periods.</p>
-        <p>Below, you can explore each scene in chronological order of the timeline, or in the aired episode order.</p>
+        <p>The TV Series <a href="https://en.wikipedia.org/wiki/Dark_(TV_series)">Dark</a> has a complex storyline that takes place over several time periods.</p>
+        <p>Below, you can explore each scene in chronological order of the timeline, or in the televised episode order.</p>
       </div>
 
       <Timeline />
