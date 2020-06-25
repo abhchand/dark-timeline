03a881d592d6582e24e2c6ceebf10c619e3ae96a
abhchand <me@abhchand.me>
2020-06-25 02:05:26 -0400

    Fix placeholder reference

--- src/Timeline/Scenes/Modal/Modal.jsx
+++ src/Timeline/Scenes/Modal/Modal.jsx
@@ -55,7 +55,7 @@ class Modal extends React.Component {
   }
 
   renderImages(scene) {
-    const urls = scene.images ? scene.images.map(i => i.medium) : ['/screenshots/placeholder.jpg'];
+    const urls = scene.images ? scene.images.map(i => i.medium) : ['/screenshots/placeholder.png'];
 
     return urls.map((url) => {
       return <a target="_blank" href={url} ><img src={url} /></a>;
